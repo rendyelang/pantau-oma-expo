eda565dce43547181d76e71629a711886c4601d6
rendyelang <lsmnrndy123@gmail.com>
2026-02-05 10:05:43 +0700

    feat: Add Firebase configuration, ignore environment files, and update EAS preview build settings.

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkfW40MafJFzzj5g8QPBfo3GjW5VrhbI0",
-  authDomain: "[pantau-oma.firebaseapp.com](http://pantau-oma.firebaseapp.com/)",
-  projectId: "pantau-oma",
-  storageBucket: "pantau-oma.firebasestorage.app",
-  messagingSenderId: "75395527174",
-  appId: "1:75395527174:web:ddac264038bb3a4a9d4987",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
